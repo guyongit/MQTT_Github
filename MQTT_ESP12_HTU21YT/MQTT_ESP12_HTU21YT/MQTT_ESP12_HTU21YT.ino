3f1c841d73901f068483a5a8d875899bb8ad0b78
guyongit <guy.limpas@gmail.com>
2017-12-11 15:34:55 +0100

    Projet youtube MQTT

---
+++ MQTT_ESP12_HTU21YT/MQTT_ESP12_HTU21YT/MQTT_ESP12_HTU21YT.ino
@@ -0,0 +1,139 @@
+/*
+Name:		MQTT_ESP12_HTU21
+Created:	27/11/2017 12:35:40
+Auteur:	limpas guy
+
+Projet objet connecté avec MQTT + Node-RED
+
+*/
+
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>			// Librairie associée au HTU21D
+#include <SparkFunHTU21D.h> // Librairie capteur HTU21D
+
+#define wifi_ssid "TP-LINK_GUY_LAB" // "Votre Nom_du_routeur"
+#define wifi_password "48035866"	// "Votre Mot_de_passe_du_routeur"
+
+#define mqtt_server "192.168.0.101"
+// #define mqtt_user "guest"     //s'il a été configuré sur Mosquitto
+// #define mqtt_password "guest" //idem
+
+#define temperature_topic "stationEsp12/temperature"  //Topic température
+#define humidity_topic "stationEsp12/humidite"        //Topic humidité
+
+//Buffer qui permet de décoder les messages MQTT reçus
+char message_buff[100];
+
+long lastMsg = 0;   //Horodatage du dernier message publié sur MQTT
+					// long lastRecu = 0;
+bool debug = false;  //Affiche sur la console si True
+
+HTU21D htu;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup() {
+	Serial.begin(115200);
+	setup_wifi();						 //On se connecte au réseau wifi
+	client.setServer(mqtt_server, 1883); //Configuration de la connexion au serveur MQTT
+	client.setCallback(callback);		//La fonction de callback qui est executée à chaque réception de message   
+	htu.begin();  // Initialisation HTU21D
+}
+
+//Connexion au réseau WiFi:
+void setup_wifi() {
+	delay(10);
+	Serial.println();
+	Serial.print("Connexion a ");
+	Serial.println(wifi_ssid);
+
+	WiFi.begin(wifi_ssid, wifi_password);
+
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial.print(".");
+	}
+
+	Serial.println("");
+	Serial.println("Connexion WiFi etablie ");
+	Serial.print("=> Addresse IP : ");
+	Serial.println(WiFi.localIP());
+}
+
+//Reconnexion :
+void reconnect() {
+	//Boucle jusqu'à obtenur une reconnexion
+	while (!client.connected()) {
+		Serial.print("Connexion au serveur MQTT...");
+		// si MQTT sécurisé alors (client.connect("ESP8266Client", mqtt_user, mqtt_password)).
+		if (client.connect("ESP8266Client")) {
+			Serial.println("connected");
+			//Une fois connecté, on peut s'inscrire à un Topic
+			//par exemple : client.subscribe("outTopic/iotTopic")
+		}
+		else {
+			Serial.print("failed, erreur : ");
+			Serial.print(client.state());
+			Serial.println("5 secondes avant de recommencer");
+			delay(5000);
+		}
+	}
+}
+
+void loop() {
+	if (!client.connected()) {
+		reconnect();
+	}
+	client.loop();
+
+	long now = millis();
+	//Envoi des mesures toutes les 5000mS
+	if (now - lastMsg > 5000) {
+		lastMsg = now;
+		//Lecture de l'humidité ambiante
+		float humd = htu.readHumidity();
+		// Lecture de la température en Celcius
+		float temp = htu.readTemperature();
+
+		if (debug) {
+			Serial.print("Temperature : ");
+			Serial.print(temp);
+			Serial.print(" | Humidite : ");
+			Serial.println(humd);
+		}
+		//Publication des mesures:
+		client.publish(temperature_topic, String(temp).c_str(), true);
+		client.publish(humidity_topic, String(humd).c_str(), true);      //et l'humidité
+	}
+}
+
+// Déclenche les actions à la réception d'un message
+void callback(char* topic, byte* payload, unsigned int length) {
+
+	int i = 0;
+	if (debug) {
+		Serial.println("Message recu =>  topic: " + String(topic));
+		Serial.print(" | longueur: " + String(length, DEC));
+	}
+	/* Lignes de code pour traitement des topics reçus :
+	for (i = 0; i<length; i++) {
+	message_buff[i] = payload[i];
+	}
+	message_buff[i] = '\0';
+
+	String msgString = String(message_buff);
+	if (debug) {
+	Serial.println("Payload: " + msgString);
+	}
+
+	if (msgString == "ON") {
+	digitalWrite(D2, HIGH);
+	}
+	else {
+	digitalWrite(D2, LOW);
+	}
+	*/
+}
---
+++ MQTT_Uno_BME280YT/MQTT_Uno_BME280YT/MQTT_Uno_BME280YT.ino
@@ -0,0 +1,172 @@
+/*
+Name:		MQTT_uno_BME280.ino
+Created:	29/11/2017 07:08:26
+Author:	limpas guy
+*/
+
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include <SPI.h>
+#include <Ethernet.h>
+#include <PubSubClient.h>
+
+Adafruit_BME280 bme;  // I2C BME280
+
+int led_Bleu = 7;
+int led_Vert = 6;
+int led_Red = 5;
+int led_Jaune = 3;
+
+// Mettre à jour les 3 lignes suivantes selon votre configuration réseau:
+byte mac[] = { 0x90, 0xA2, 0xDA, 0x0E, 0xD1, 0xA6 }; // mac adress shield Ethernet
+IPAddress ip(192, 168, 0, 120);		 // (192, 168, 0, 120) adresse IP carte UNO
+IPAddress server(192, 168, 0, 101);   //(192, 168, 0, 101);  // Broker
+
+//#define mqtt_user "moi"         // Si configuré sur le Broker !
+//#define mqtt_password "motdepasse" // Si configuré sur le Broker !
+
+
+
+long lastMsg = 0;
+int value = 0;
+char message_buff[100];
+
+// Fonction appelée lors de la réception d'un topic + traitement du payload:
+void callback(char* topic, byte* payload, unsigned int length)
+{
+	int i = 0;
+	Serial.print("Message arrived [");
+	Serial.print(topic);
+	String topic2 = String(topic);
+	Serial.println("] ");
+	// Traitement du payload, on le transforme en String.
+	for (i = 0; i<length; i++)
+	{
+		message_buff[i] = payload[i];
+	}
+	message_buff[i] = '\0';
+	String msgString = String(message_buff);
+	Serial.println("Payload: " + msgString);
+	if (topic2 == "stationUno/switchBleu") // LED bleue
+	{
+		if (msgString == "ON") {
+			digitalWrite(led_Bleu, HIGH);
+		}
+		else {
+			digitalWrite(led_Bleu, LOW);
+		}
+	}
+	if (topic2 == "stationUno/switchVert") // LED verte
+	{
+		if (msgString == "ON") {
+			digitalWrite(led_Vert, HIGH);
+		}
+		else {
+			digitalWrite(led_Vert, LOW);
+		}
+	}
+	if (topic2 == "stationUno/switchRouge") // LED rouge
+	{
+		if (msgString == "ON") {
+			digitalWrite(led_Red, HIGH);
+		}
+		else {
+			digitalWrite(led_Red, LOW);
+		}
+	}
+	if (topic2 == "stationUno/switchJaune") // LED Jaune
+	{
+		if (msgString == "ON") {
+			digitalWrite(led_Jaune, HIGH);
+		}
+		else {
+			digitalWrite(led_Jaune, LOW);
+		}
+	}
+}
+
+EthernetClient ethClient;
+PubSubClient client(ethClient);
+
+void reconnect() {
+	// Loop until we're reconnected
+	while (!client.connected()) {
+		Serial.print("Attempting MQTT connection...");
+		// On attend la connection, mqtt_user et mqtt_password si configuré sur le Broker !
+		// (client.connect("arduinoClient", mqtt_user, mqtt_password))
+		if (client.connect("arduinoClient")) {
+			Serial.println("connected");
+			// une fois connectée, on souscrit au différent topic:
+			client.subscribe("stationUno/switchBleu");
+			client.subscribe("stationUno/switchVert");
+			client.subscribe("stationUno/switchRouge");
+			client.subscribe("stationUno/switchJaune");
+
+		}
+		else {
+			Serial.print("failed, rc=");
+			Serial.print(client.state());
+			Serial.println(" try again in 5 seconds");
+			// On attend 5 secondes pour essayer de se reconnecter au Broker
+			delay(5000);
+		}
+	}
+}
+
+
+void setup()
+{
+	Serial.begin(115200);
+	bme.begin();
+
+	// Configuration des pins Arduino en sortie:
+	pinMode(led_Bleu, OUTPUT);
+	pinMode(led_Vert, OUTPUT);
+	pinMode(led_Red, OUTPUT);
+	pinMode(led_Jaune, OUTPUT);
+
+	client.setServer(server, 1883);
+	client.setCallback(callback);
+
+	Ethernet.begin(mac, ip);
+	// Allow the hardware to sort itself out
+	delay(1500);
+}
+
+void loop()
+{
+	if (!client.connected()) {
+		reconnect();
+	}
+	client.loop();
+
+	long now = millis();
+	if (now - lastMsg > 5000) // toute les 5 secondes on envoi les mesures au Broker
+	{
+		char t[10];
+		lastMsg = now;
+		++value;
+		// On envoi les mesures au Broker:
+		float temp_U = bme.readTemperature();
+		dtostrf(temp_U, 3, 2, t);  // Conversion float en char.
+		client.publish("stationUno/temperature", t);
+
+		float hum_U = bme.readHumidity();
+		dtostrf(hum_U, 3, 2, t);
+		client.publish("stationUno/humidite", t);
+
+		float press_U = bme.readPressure();
+		dtostrf(press_U, 0, 0, t);
+		client.publish("stationUno/pression", t);
+
+		float altit_U = bme.readAltitude(1013.25);
+		dtostrf(altit_U, 3, 2, t);
+		client.publish("stationUno/altitude", t);
+
+		Serial.println("Publish message du BME280");
+
+	}
+}
+
